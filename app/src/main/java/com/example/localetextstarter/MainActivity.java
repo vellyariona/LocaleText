f58b016ad3dbeda8c0620815541307008889b42d
vellyariona <vellya.riona@mail.ugm.ac.id>
2023-03-27 14:00:03 +0700

    1

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -9,8 +9,13 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
                 showHelp();
             }
         });
+
+//        menentukan tanggal kadaluarsa
+        Date myDate = new Date();
+        long expirationDate = myDate.getTime() +
+                TimeUnit.DAYS.toMillis(5);
+        myDate.setTime(expirationDate);
+
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredTextView = findViewById(R.id.date);
+        expiredTextView.setText(formatDate);
     }
 
     /**
